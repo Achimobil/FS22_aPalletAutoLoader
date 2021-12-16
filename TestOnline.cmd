b989723636d0a00cbf625f5c464b40a68a8feb24
Achimobil79 <achimobil@hotmail.com>
2021-12-17 00:06:39 +0100

    TestOnline.cmd ohne intro

--- TestOnline.cmd
+++ TestOnline.cmd
@@ -1,2 +1,2 @@
 "C:\Program Files\7-Zip\7z.exe" a -tzip "e:\Achim\Documents\My Games\FarmingSimulator2022\mods\FS22_aPalletAutoLoader.zip" @FileDefinition.txt
-"F:\Program Files (x86)\Farming Simulator 2022\FarmingSimulator2022.exe" -cheats
+"F:\Program Files (x86)\Farming Simulator 2022\FarmingSimulator2022.exe" -cheats -skipStartVideos
