fc04ef003dc3d1da58a2f8b7a20f2d562806584f
Achim Kuntz <achimobil@hotmail.com>
2022-11-24 23:35:58 +0100

    LS22ModTest moved

--- TestOnline.cmd
+++ TestOnline.cmd
@@ -1,2 +1,2 @@
-"C:\Program Files\7-Zip\7z.exe" a -tzip "E:\LS22ModTest\mods\FS22_aPalletAutoLoader.zip" @FileDefinition.txt
-"E:\Program Files (x86)\Farming Simulator 2022\FarmingSimulator2022.exe" -cheats -skipStartVideos -profile E:\LS22ModTest
+"C:\Program Files\7-Zip\7z.exe" a -tzip "E:\LS22Profile\LS22ModTest\mods\FS22_aPalletAutoLoader.zip" @FileDefinition.txt
+"E:\Program Files (x86)\Farming Simulator 2022\FarmingSimulator2022.exe" -cheats -skipStartVideos -profile E:\LS22Profile\LS22ModTest
