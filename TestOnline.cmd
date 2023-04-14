f8bf0eece39939fe2e178a494adbb94ca4867a5c
Achim Kuntz <achimobil@hotmail.com>
2023-04-14 15:13:08 +0200

    cmd anpassung

--- TestOnline.cmd
+++
@@ -1,2 +0,0 @@
-"C:\Program Files\7-Zip\7z.exe" a -tzip "E:\LS22Profile\LS22ModTest\mods\FS22_aPalletAutoLoader.zip" @FileDefinition.txt
-"E:\Program Files (x86)\Farming Simulator 2022\FarmingSimulator2022.exe" -cheats -skipStartVideos -profile E:\LS22Profile\LS22ModTest -devWarnings
