80262bc45534c0a7636fa93013700c1ad706716f
Achim Kuntz <achimobil@hotmail.com>
2023-06-09 14:19:56 +0200

    cmds auf ignore gestellt

--- Test.Ostsee.3.cmd
+++
@@ -1,2 +0,0 @@
-"C:\Program Files\7-Zip\7z.exe" a -tzip "E:\LS22Profile\Ostseekueste\mods\FS22_aPalletAutoLoader.zip" @FileDefinition.txt
-"C:\Program Files (x86)\Farming Simulator 2022\FarmingSimulator2022.exe" -autoStartSavegameId 3 -cheats -profile E:\LS22Profile\Ostseekueste -devWarnings
