8c3d24449ac80e46672c7094fbe25cc1f6d37ae6
Achim Kuntz <achimobil@hotmail.com>
2023-06-16 12:16:57 +0200

    Übersetzung des Ladeguts nur, wenn noch nicht gesetzt, verhindert aber die LogWarning nicht

--- APalletAutoLoader.lua
+++ APalletAutoLoader.lua
@@ -712,7 +712,9 @@ function APalletAutoLoader:onLoad(savegame)
 			local autoLoadObject = {}
 			autoLoadObject.index = spec.loadArea["baseNode"]
 			autoLoadObject.name = name
-			autoLoadObject.nameTranslated = g_i18n:getText("aPalletAutoLoader_" .. name)
+			if autoLoadObject.nameTranslated == nil then
+				autoLoadObject.nameTranslated = g_i18n:getText("aPalletAutoLoader_" .. name)
+			end
 			APalletAutoLoader:AddSupportedObjects(autoLoadObject, name)
 			
 			if autoLoadObject.sizeX == nil then
